3807242c76228bc110a35d4699a983f8ff935565
GreenCherry1 <137512652+GreenCherry1@users.noreply.github.com>
2023-06-27 14:39:34 +0300

    Update UPSUpdater.py

--- UPSUpdater.py
+++ UPSUpdater.py
@@ -5,7 +5,8 @@ import win32com.client as win32
 
 def CreateEmptyXlsFile(path):
     '''Creates empty xls file.
-    path - path of the file'''
+    Arquments: path - path of the file
+    returns: None'''
     wb=xlwt.Workbook()
     wb.add_sheet('1')
     wb.save(path)
@@ -13,7 +14,8 @@ def CreateEmptyXlsFile(path):
     
 def DownloadXlsFile(zipzone,path,workbook):
     '''Writes .xls file from the server content in existing file
-    Arquments: zipzone = 3 first numbers of the zipcode. path - path of the file. workbook - file's workbook'''
+    Arquments: zipzone = 3 first numbers of the zipcode. path - path of the file. workbook - file's workbook
+    returns: None'''
     headers = { 'User-Agent': 'Chrome/92.0.4515.107'}
     resp=requests.get(url+zipzone+'.xls',headers=headers)
     open(path,'wb').write(resp.content)
@@ -21,7 +23,8 @@ def DownloadXlsFile(zipzone,path,workbook):
 
 def ConvertXlsToXlsx(uploadpath,downloadpath):
     '''Converts .xls file to .xlxs file 
-    uploadpath - path of the .xls file. downloadpath - path and name where to save .xlsx file'''
+    Arquments: uploadpath - path of the .xls file. downloadpath - path and name where to save .xlsx file
+    returns: None'''
     excel = win32.gencache.EnsureDispatch('Excel.Application')
     wb = excel.Workbooks.Open(uploadpath)
     wb.SaveAs(downloadpath, FileFormat = 51)    #FileFormat = 51 is for .xlsx
@@ -29,9 +32,11 @@ def ConvertXlsToXlsx(uploadpath,downloadpath):
     excel.Application.Quit()
 
 def UpdateZip(ZipCodeNumber,worksheet):
-    '''copies zip codes of downloaded zones file to cell in Carriers zone range'''
-    ws.cell(row = ZipCodeNumber, column = 2).value=s[39:42]+s[43:45]
-    ws.cell(row = ZipcodeNumber, column = 3).value=s[49:52]+s[53:55]
+    '''copies zip codes of downloaded zones file to cell in Carriers zone range
+     Arquments: ZipCodeNumber - number of row in Carriers zone range. worksheet - worksheet in Carriers zone range
+     returns: None'''
+    worksheet.cell(row = ZipCodeNumber, column = 2).value=s[39:42]+s[43:45]
+    worksheet.cell(row = ZipcodeNumber, column = 3).value=s[49:52]+s[53:55]
 
 url = 'https://www.ups.com/media/us/currentrates/zone-csv/'
 wb=openpyxl.load_workbook("Carriers zone range.xlsx")
